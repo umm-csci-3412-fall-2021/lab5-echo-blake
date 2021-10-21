946f5286106fc651d23a3f789e4f758e5e4be6d0
Blake Johnson <joh18484@tardis.morris.umn.edu>
2021-10-21 11:29:12 -0500

    Added lines for both EchoClient and EchoServer Added lines for reading and writing to a created socket.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,31 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) throws IOException {
+        String server;
+        server = args[0];
+
+        try {
+            Socket echoSocket = new Socket(server, portNumber);
+            InputStream input = echoSocket.getInputStream();
+            String reader = input.read(System.in);
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (ConnectException ce) {
+          System.out.println("We were unable to connect to " + server);
+          System.out.println("You should make sure the server is running.");
+        } catch (IOException ioe) {
+          System.out.println("We caught an unexpected exception");
+          System.err.println(ioe);
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,28 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+
+   public static final int portNumber = 6013;
+
+   public static void main(String[] args) {
+       try {
+           ServeSocket socket = new ServerSocket(portNumber);
+
+           while (true) {
+               Socket client = socket.accept();
+               System.out.println("Got a request!");
+
+               OutputStream output = client.getOutputStream();
+               output.write(System.out);
+
+               client.close();
+           }
+           
+       } catch (IOException ioe) {
+         System.out.println("We caught an unexpected exception");
+         System.err.println(ioe);
+       }
+   } 
 }
